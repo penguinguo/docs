7c2aa58ec0042cc6445a9b9f3c25138573316277
Hampus Sandberg <hampus.sandberg@cern.ch>
2020-02-15 16:51:53 +0100

    Added page with ESP projects.

---
+++ docs/electronics/esp-boards/esp8266-led-controller-board/index.md
@@ -0,0 +1,7 @@
+# ESP8266-LED-Controller-Board
+- [GitHub repository <i class="fa fa-external-link"></i>](https://github.com/hampussandberg/esp-boards/tree/master/ESP8266-LED-Controller-Board)
+- [Interactive BOM](ESP8266-LED-Controller-Board-ibom.html){target="\_blank"}
+
+[![](3d-top.png){style="width:100%"}](3d-top.png)
+
+Interactive BOM created using: https://github.com/openscopeproject/InteractiveHtmlBom
---
+++ get-external.sh
@@ -0,0 +1,4 @@
+# ESP8266-LED-Controller-Board
+wget -O docs/electronics/esp-boards/esp8266-led-controller-board/index.md https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/README.md
+wget -O docs/electronics/esp-boards/esp8266-led-controller-board/ESP8266-LED-Controller-Board-ibom.html https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/ESP8266-LED-Controller-Board-ibom.html
+wget -O docs/electronics/esp-boards/esp8266-led-controller-board/3d-top.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/3d-top.png
