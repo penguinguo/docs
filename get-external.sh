fef34c2d6d4bd73ecf339ccb9a8ab37df8fd4783
Hampus Sandberg <hampus.sandberg@cern.ch>
2020-03-31 11:47:02 +0200

    Updated externals-.

--- get-external.sh
+++ get-external.sh
@@ -1,10 +1,10 @@
 # ESP8266-LED-Controller-Board
-wget -O docs/electronics/esp-boards/esp8266-led-controller-board/index.md https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/README.md
-wget -O docs/electronics/esp-boards/esp8266-led-controller-board/ESP8266-LED-Controller-Board-ibom.html https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/ESP8266-LED-Controller-Board-ibom.html
-wget -O docs/electronics/esp-boards/esp8266-led-controller-board/3d-top.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/3d-top.png
+wget --no-cache -O docs/electronics/esp-boards/esp8266-led-controller-board/index.md https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/README.md
+wget --no-cache -O docs/electronics/esp-boards/esp8266-led-controller-board/ESP8266-LED-Controller-Board-ibom.html https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/ESP8266-LED-Controller-Board-ibom.html
+wget --no-cache -O docs/electronics/esp-boards/esp8266-led-controller-board/3d-top.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-LED-Controller-Board/3d-top.png
 
 # ESP8266-Cherry-MX-Board
-wget -O docs/electronics/esp-boards/esp8266-cherry-mx-board/index.md https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/README.md
-wget -O docs/electronics/esp-boards/esp8266-cherry-mx-board/ESP8266-Cherry-MX-Board-ibom.html https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/ESP8266-Cherry-MX-Board-ibom.html
-wget -O docs/electronics/esp-boards/esp8266-cherry-mx-board/3d-top.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/3d-top.png
-wget -O docs/electronics/esp-boards/esp8266-cherry-mx-board/3d-bottom.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/3d-bottom.png
+wget --no-cache -O docs/electronics/esp-boards/esp8266-cherry-mx-board/index.md https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/README.md
+wget --no-cache -O docs/electronics/esp-boards/esp8266-cherry-mx-board/ESP8266-Cherry-MX-Board-ibom.html https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/ESP8266-Cherry-MX-Board-ibom.html
+wget --no-cache -O docs/electronics/esp-boards/esp8266-cherry-mx-board/3d-top.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/3d-top.png
+wget --no-cache -O docs/electronics/esp-boards/esp8266-cherry-mx-board/3d-bottom.png https://raw.githubusercontent.com/hampussandberg/esp-boards/master/ESP8266-Cherry-MX-Board/3d-bottom.png
